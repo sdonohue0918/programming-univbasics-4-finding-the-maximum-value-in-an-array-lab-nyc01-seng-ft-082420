63efcd315b7b3014bdff5fb63d827ad1ec8c45ed
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-23 01:12:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,12 @@
 def find_max_value(array)
   count = 0
+  max_value = 0
   while count < array.length do
-      array.max {|a,b| a.attr <=> b.attr }
+    if max_value < array[count]
+      max_value = array[count]
+    end
+
     count += 1
   end
+  max_value
 end
